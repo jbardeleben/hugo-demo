ba0628e41bcf203fcd37d8a8258ba92b2d140a8e
Jay Bardeleben <jbardeleben@gmail.com>
2020-01-09 11:00:19 -0500

    create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * HUGO BLOGGING PLATFORM TESTING PROJECT
+ * _____________________________________________________________________________
+ *
+ * @author Jay Bardeleben <jbardeleben@gmail.com>
+ * @copyright (c) 2020 Jay Bardeleben
+ * @license MIT (see README.md @link ./README.md)
+ */
+
+?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Hugo Blog Test</title>
+
+  <!--
+  <link rel="icon" href="./favicon.ico">
+  <link href="./css/styles.css" rel="stylesheet">
+  -->
+
+</head>
+<body>
+
+  <main>
+    <h1>Hugo Blog Test</h1>
+    <hr>
+    <p>
+      This is a test project to learn how to use the Hugo blogging platform. 
+      Nothing special to see here for now, but I will be adding over time as
+      I work through the tutorial from Flavio Copes.
+    </p>
+    <p>
+      <strong style="text-decoration:underline">Resources</strong>
+      <br>
+      <a href="https://flaviocopes.com" rel="nofollow">Flavio Copes Homepage</a>
+      <a href="https://flaviocopes.com/start-blog-with-hugo/" rel="nofollow">Flavio Copes Hugo Tutorial</a>
+    </p>
+  </main>
+
+<footer class="footer">
+    <div class="">
+      &copy; <?php echo \date("Y"); ?> jBardeleben
+    </div>
+  </footer>
+
+</body>
+</html>
